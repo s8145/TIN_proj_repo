09a85880e574e4c4b2456c5a299ec2c96ae3657e
filip <s8145@pjwstk.edu.pl>
2013-01-30 14:02:13 +0100

    works 1

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var http = require('http'),
     path = require('path'),
     io = require('socket.io');
 
+   
+
 var server = http.createServer(function (req, res) {
     'use strict';
     var filePath = '.' + req.url,
@@ -23,6 +25,8 @@ var server = http.createServer(function (req, res) {
         contentType = 'text/css';
         break;
     }
+
+
 
     path.exists(filePath, function (exists) {
         if (exists) {
@@ -52,23 +56,85 @@ var socket = io.listen(server);
  */
 
 var task = [
-    {nazwa: "Wyslac FV", data: "2013-01-31", user: 'filip', status: 0},
-    {nazwa: "17ta, sms szef", data: "2013-01-31", user: 'filip', status: 0},
-    {nazwa: "Pion, odebrac dokumenty", data: "2013-01-31", user: 'monika', status: 0},
-    {nazwa: "Bualoy visa", data: "2013-01-23", user: 'monika', status: 1},
-    {nazwa: "Wyslac raport", data: "2013-01-31", user: 'monika', status: 0},
-    {nazwa: "Poczta", data: "2013-01-25", user: 'monika', status: 1}
-	{nazwa: "Nadac paczke", data: "2013-02-02", user: 'wszyscy', status: 0}
-	{nazwa: "Podlac kwiatki", data: "2013-01-31", user: 'wszyscy', status: 0}
+    {nazwa: "Wyslac FV", data: "2013-01-31", time: "15:00", user: 'Filip', status: 0},
+    {nazwa: "17ta, sms szef", data: "2013-01-31", time: "13:30", user: 'Filip', status: 0},
+    {nazwa: "Pion, odebrac dokumenty", data: "2013-01-31", time: "20:00", user: 'Monika', status: 0},
+    {nazwa: "Bualoy visa", data: "2013-01-23", time: "", user: 'Gosia', status: 1},
+    {nazwa: "Wyslac raport", data: "2013-01-31", time: "", user: 'Artur', status: 0},
+    {nazwa: "Poczta", data: "2013-01-25", time: "18:00", user: 'Ewelina', status: 1},
+	{nazwa: "Nadac paczke", data: "2013-02-02", time: "18:00", user: 'wszyscy', status: 0},
+	{nazwa: "Podlac kwiatki", data: "2013-01-31", time: "", user: 'wszyscy', status: 0}
 ];
 
 var users = [
-    {nazwa: 'filip'},
-    {nazwa: 'monika'}
+    {nazwa: 'Filip'},
+    {nazwa: 'Monika'},
+	{nazwa: 'Marcin'},
+	{nazwa: 'Artur'},
+	{nazwa: 'Gosia'},
+	{nazwa: 'Angelika'},
+	{nazwa: 'Ewelina'}
 ];
 
-socket.on('connection', function (client) {
+var swch = 0;
 
+
+
+
+	
+socket.on('connection', function (client) {
+    'use strict';
+    swch = 0;
+
+    client.on('setUser', function(data){//sprawdzanie czy logowany uzytkownik znajduje sie w tablicy
+        for(var i=0; i<users.length; i++){
+            if(users[i].nazwa===data){
+                swch = 1;
+            }
+        }
+        if(swch==1){
+            client.emit('newTask', task);//wyswietla zadania z tablicy task
+        }else{
+            client.emit('zlyLogin', "zła nazwa użytkownika");
+        }
+    }); 
+
+    client.on('addTask', function (data){
+        task.push(data);//dodaje do tablicy nowe zadanie
+        client.broadcast.emit('newTask', task);
+        client.emit('newTask', task);
+    });
+
+    client.on('change', function (data){
+        if(task[data].status===0){
+            task[data].status=1;
+            client.emit('newTask', task);
+            client.broadcast.emit('newTask', task);
+        }else{
+            task[data].status=0;
+            client.emit('newTask', task);
+            client.broadcast.emit('newTask', task);
+        }
+    });
+
+    client.on('delete', function(data){
+        task.splice(data, 1);
+        client.emit('newTask', task);
+        client.broadcast.emit('newTask', task);
+    });
+	
+	
+	function updateTime() {
+		var teraz = new Date();
+		var terazStr = teraz.getHours()+':'+teraz.getMinutes()+':'+teraz.getSeconds();
+		client.broadcast.emit('newTime', terazStr)
+	}	
+	setInterval(updateTime, 1000);
+	
+
+
+	
+	
 });
 
 server.listen(3030);
--- skrypt.js
+++ skrypt.js
@@ -2,9 +2,103 @@
 /*global io: false */
 $(document).ready(function () {
     'use strict';
-    var socket = io.connect('http://localhost:3030'),
- 
+    var socket = io.connect(),
+        myName = '';
+
+    $('#addTask').hide();
+    $('#taska').hide();
+	$('#zegar').show();
+    console.log('connecting…');
+
+    socket.on('connect', function () {
+        console.log('connected!');
+    });
 
 
+    socket.on('zlyLogin', function(msg){
+        $('#login').append('<br><font size="2">'+msg+'</font>')
+    });
+	
+	socket.on('newTime', function(time){
+        $('#zegar').html('<br><font size="2">'+time+'</font>');
+    });
+	
 
+	//---------------------------------------------------------------------------
+    socket.on('newTask', function(data){
+        console.log("new task");
+        $('#active').html('');
+        $('#history').html('');
+        var dzisiaj = new Date();
+        var dzisiajStr = dzisiaj.getFullYear() + '-' + dzisiaj.getMonth() + '-' + dzisiaj.getDate();
+		//var teraz = new Date();
+		//var terazStr = teraz.getHours()+':'+teraz.getMinutes()+':'+teraz.getSeconds();
+		
+		for(var i=0; i<data.length; i++){
+            if(data[i].status===0){
+                var dataZadania = new Date(data[i].data);   
+                var dataZadaniaStr = dataZadania.getFullYear() + '-' + dataZadania.getMonth() + '-' + dataZadania.getDate();
+                
+				//var czasZadania = new Date();
+				//czasZadania.setHours(data[i].time);		
+				//var czasZadaniaStr = czasZadania.getHours()+':'+czasZadania.getMinutes()+':'+czasZadania.getSeconds();
+				
+				if(dzisiajStr>dataZadaniaStr){
+                    if(data[i].user===myName){
+                        $('#active').append('<li id="'+i+'" class="arv"><div id="pole" class="'+i+'"<p><center>'+data[i].nazwa+'</center><br><b>'+ data[i].data+'&nbsp; '+data[i].time+'</b><span class="user">'+data[i].user+'</span></p></div></li>');
+                    }else{
+                        $('#active').append('<li id="'+i+'" class="arv"><div id="pole" class="'+i+'"<p><center>'+data[i].nazwa+'</center><br><b>'+ data[i].data+'&nbsp; '+data[i].time+'</b><span class="user2">'+data[i].user+'</span></p></div></li>');
+                    }
+                }else{
+                    if(data[i].user===myName){
+                        $('#active').append('<li id="'+i+'"><div id="pole" class="'+i+'"<p><center>'+data[i].nazwa+'</center><br><b>'+ data[i].data+'&nbsp; '+data[i].time+'</b><span class="user">'+data[i].user+'</span></p></div></li>');
+                    }else{
+                        $('#active').append('<li id="'+i+'"><div id="pole" class="'+i+'"<p><center>'+data[i].nazwa+'</center><br><b>'+ data[i].data+'&nbsp; '+data[i].time+'</b><span class="user2">'+data[i].user+'</span></p></div></li>');
+                    }
+                }
+            }else{
+                $('#history').append('<li id="'+i+'"><div id="pole" class="'+i+'"><p><center>'+data[i].nazwa+'</center><br><b>'+ data[i].data+'</b><span class="user2">'+data[i].user+'</span></p></div>&nbsp;<button id="del" class="'+i+'">-</button></li>');
+            }   
+        }
+		
+		
+		
+		
+		$('#black').hide();
+        $('#addTask').show();
+        $('#taska').show();
+		
+        $("li #pole").click(function (a){
+            socket.emit('change', $(this).attr('class'));
+        });
+
+        $('li #del').click(function (){
+            socket.emit('delete', $(this).attr('class'));
+        });
+
+    });
+	//---------------------------------------------------------------------------
+	
+	
+	$('#userLogin').keypress(function(event){
+		if(event.keyCode===13){
+			socket.emit('setUser', $('#userLogin').val());//po nacisnieciu enter emituje do serwera
+			myName = $('#userLogin').val();
+		}
+	});
+
+
+  
+
+    $('#add').click(function (event){ //dodawanie zadania
+		var nazwa = $('#nazwa').val();  //przypisywanie wartosci
+        var adresat = $('#user').val();
+        var termin= $('#termin').val();
+		var godzina= $('#godzina').val();
+        var status = 0;
+        socket.emit('addTask', {nazwa: nazwa, data: termin, time: godzina, user: adresat, status: status});
+		
+    });
+
+   
 });
